dc8f88ce522a9b8b9133055f469017fb02c78bbe
aman21030 <i10v3.3.6bc@gmail.com>
2019-12-27 14:03:40 +0900

    ログインのエラーだったのをとりあえずなおした

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,6 @@
 <?php
 	echo ' <header> ';
 	echo '	 <a href="http://6101.web-seito.com/imitationcom/"><img src="http://6101.web-seito.com/imitationcom/assets/img/seminar_illust.png" alt="ロゴ"></a> ';
-	echo '	 <a href="" class="btn btn-default">ログアウト</a> ';
+	echo '	 <a href="http://6101.web-seito.com/imitationcom/login/index.php" class="btn btn-default">ログイン・新規登録</a> ';
+	// echo '	 <a href="" class="btn btn-default">ログアウト</a> ';
 	echo ' </header> ';//ヘッダーend
--- includes/islogin.php
+++ includes/islogin.php
@@ -1,5 +1,5 @@
 <?php
-	require_once dirname(__FILE__) . '/../require.php';
+	require_once dirname(__FILE__) . '/require.php';
 	$conn = new DbConn();
 
 	//セッション開始
