494a29964f483755b120e7a7cdbcf0a5e9507b34
to-aman <i10v3.3.6bc@gmail.com>
2020-01-09 02:51:52 +0900

    htmlspecialchars付けたり。

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,11 @@
 <?php
+	
+
 	echo ' <header> ';
 	echo '	 <a href="http://6101.web-seito.com/imitationcom/"><img src="http://6101.web-seito.com/imitationcom/assets/img/seminar_illust.png" alt="ロゴ"></a> ';
-	echo '	 <a href="http://6101.web-seito.com/imitationcom/login/index.php" class="btn btn-default">ログイン・新規登録</a> ';
-	// echo '	 <a href="" class="btn btn-default">ログアウト</a> ';
+	if(is_null($_SESSION['id'])){
+		echo '	 <a href="http://6101.web-seito.com/imitationcom/login/index.php" class="btn btn-default">ログイン・新規登録</a> ';
+	}elseif(isset($_SESSION['id'])){
+		echo '	 <a href="" class="btn btn-default">ログアウト</a> ';
+	}
 	echo ' </header> ';//ヘッダーend
